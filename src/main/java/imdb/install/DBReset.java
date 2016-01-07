5700339632308028ddc3a57aec5220ee86c4d9ee
anushyakkutty <anushya.k.kutty@gmail.com>
2016-01-07 13:58:16 +0530

    Deleting thumbnails during dbreset

--- src/main/java/imdb/install/DBReset.java
+++ src/main/java/imdb/install/DBReset.java
@@ -1,10 +1,26 @@
 package imdb.install;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 import imdb.utils.DBUtils;
+import imdb.utils.PropertyFileParser;
 
 public class DBReset {
 
 	public static void main(String args[]) {
+		cleanDBAndMetaData();		
+	}
+
+	public static void cleanDBAndMetaData() {
 		DBUtils.executeMysqlScript("Dbreset.sql");
+		try {
+			FileUtils.cleanDirectory(new File(PropertyFileParser.THUMB_NAIL_DIR));
+		} catch (IOException e) {
+			System.out.println("Exception while deleting thumbnails : " + e.getMessage());
+		}
+		
 	}
 }
