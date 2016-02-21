9800c25130fbf3f4513dbd531f8a6f1b011483c9
anushyakkutty <anushya.k.kutty@gmail.com>
2016-02-21 21:36:42 +0530

    UI changes to list , add favorites  and change icon accordingly.

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -6,6 +6,7 @@ var allMovies;
 var unFavoriteIcon = "glyphicon glyphicon-star gi-1x none"
 var favoriteIcon = "glyphicon glyphicon-star gi-1x gold"
 
+
 var showPosters = function() {
 
 	$.each(allMovies, function(index, el) {
@@ -15,7 +16,13 @@ var showPosters = function() {
 		textToInsert += '<a href="#">' + '<span class="glyphicon glyphicon-bookmark gi-3x bookmark" title="Add to watchlist" data-wl-id=' + el.id +'></span></a>';
 		
 		textToInsert += '<div class="caption">';
-		textToInsert += '<span class= "' + unFavoriteIcon + '" onclick="tryFavoriting(this , '+ el.id + ')" data-fav-id=' + el.id +'><span class="rating">' +' ' +el.imdbRating  +'<span class="ten">/10</span>' +'</span></span>';
+		var favIconToBeUsed;
+		if(el.favorite == true) {
+			favIconToBeUsed = favoriteIcon;
+		} else {
+			favIconToBeUsed = unFavoriteIcon;
+		}
+		textToInsert += '<span class= "' + favIconToBeUsed + '" onclick="tryFavoriting(this , '+ el.id + ')" data-fav-id=' + el.id +'><span class="rating">' +' ' +el.imdbRating  +'<span class="ten">/10</span>' +'</span></span>';
 		
 		//textToInsert += '<a href="#">' + '<span class="glyphicon glyphicon-plus gi-2x"></span></a>';
 		//textToInsert += '<a href="#" >' + '<span class="glyphicon glyphicon-heart gi-3x"></span></a>';
@@ -35,7 +42,7 @@ var showPosters = function() {
 function tryFavoriting(param, id) {
 	if (param.className == "glyphicon glyphicon-star gi-1x gold" ) {
 		
-   
+	
 	 
 	} else {
 		param.className = "glyphicon glyphicon-star gi-1x none";
